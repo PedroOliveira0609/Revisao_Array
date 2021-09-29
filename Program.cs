d84fb7a3d255bd1bf64d2998cd7a0b2126292f03
PedroOliveira0609 <90793896+PedroOliveira0609@users.noreply.github.com>
2021-09-28 21:47:49 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArrayRevisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            string nime = string.Empty;
+
+            string[] anime = new string[10];
+            anime[0] = "One piece";
+            anime[1] = "Bleach";
+            anime[2] = "High school dxd";
+            anime[3] = "Attack on Titan";
+            anime[4] = "Hunter × Hunter";
+            anime[5] = "Castlevania";
+            anime[6] = "Pokémon";
+            anime[7] = "Super onze";
+            anime[8] = "Dinosauro rei";
+            anime[9] = "Blue Exorcist";
+
+            if (nime == "anime");
+            {
+                Console.WriteLine("A lista de anime é:");
+                for (int i = 0; i < anime.Length; i++)
+                {
+                    Console.WriteLine($"Animes: {anime[i]}");
+
+                }
+            }
+        }
+    }   
+}
